9a5b5fbc4cc88b767e704b83b24f9bb7ad2e522a
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-07-25 21:27:27 +0000

    Added models and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+	has_many :categories
+
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, :uniqueness => {:case_sensitive => false}, presence: true
+	validates :password, presence: true, confirmation: true, length:{minimum:6}
+	validates :password_confirmation, presence: true 
+
+
+
 end
