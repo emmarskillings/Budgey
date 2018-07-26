af634286b90e370acab50dcdfa769a8276d4d145
Eliza Moore <elzmoore13@gmail.com>
2018-07-26 18:19:36 +0000

    set up for axios calls

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,23 @@ import Navbar from './components/Navbar';
 import ExpenseBoard from './components/ExpenseBoard';
 import Summary from './components/Summary';
 import IncomeBoard from './components/IncomeBoard';
+import axios from 'axios'; //import for every compenent that needs a call to database
 
 class App extends Component {
+
+  componentDidMount() {
+  axios
+    .get("http://localhost:3001/api/v1/resources.json")
+    .then(response => {
+      console.log(response);
+      this.setState({
+        resources: response.data
+      });
+    })
+    .catch(error => console.log(error));
+  }
+
+
   render() {
     return (
       <div>
