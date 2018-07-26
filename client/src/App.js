8cd524c4a28336c5fa7715f54b26e4402a6cdd09
Eric Joo <ericjaeminjoo@gmail.com>
2018-07-26 15:50:58 +0000

    Setup base application react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { Container } from "reactstrap";
 import Navbar from './components/Navbar';
+import ExpenseBoard from './components/ExpenseBoard';
+import Summary from './components/Summary';
+import IncomeBoard from './components/IncomeBoard';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <div className="container">
-          <h1>Budgey</h1>
-        </div>
+
+        <Container>
+          <ExpenseBoard />
+          <Summary />
+          <IncomeBoard />
+        </Container>
       </div>
     );
   }
