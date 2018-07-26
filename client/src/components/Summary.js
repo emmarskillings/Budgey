b353f1c415c838bc6b4d7305b12259f016b273f5
Eric Joo <ericjaeminjoo@gmail.com>
2018-07-26 16:45:33 +0000

    Removed SCSS due to react coding convention and using CSS per react component

--- client/src/components/Summary.js
+++ client/src/components/Summary.js
@@ -3,7 +3,7 @@ import React from "react";
 class Summary extends React.Component {
   render() {
     return (
-      <div>
+      <div className="summary-board">
         Summary content here.
       </div>
     )
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("react-root"));
