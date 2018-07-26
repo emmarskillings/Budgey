fd9aa8e8cc0e9a370902d976e1b6062c9b0807aa
Eric Joo <ericjaeminjoo@gmail.com>
2018-07-26 15:06:52 +0000

    Create navigation bar react component baseplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
-      
+      <div>
+        <Navbar />
+        <div className="container">
+          <h1>Budgey</h1>
+        </div>
+      </div>
     );
   }
 }
