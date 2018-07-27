db28a17041fdbc394ee2807e673a331e7f080420
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-07-27 14:47:11 +0000

    Testing pie charts

---
+++ client/src/components/CategoryContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+
+class CategoryContainer extends Component {
+  render() {
+    return (
+    
+      <div>
+        Category
+      </div>
+
+      
+    )
+  }
+}
+
+export default CategoryContainer
