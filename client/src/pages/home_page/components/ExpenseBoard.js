b10436f21727995d933d2511cfc2be1baa9804c1
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-08-02 17:57:06 +0000

    Quick add button for new expense categories

--- client/src/pages/home_page/components/ExpenseBoard.js
+++ client/src/pages/home_page/components/ExpenseBoard.js
@@ -5,24 +5,39 @@ import NewExpenseModal from "./NewExpenseModal";
 import Popup from "reactjs-popup";
 import { NavLink } from "react-router-dom";
 import BarGraphs from "./BarGraphs.js";
+import NewExpenseEntry from "./NewExpenseEntry.js";
 
-const makeCardStack = cards => {
+const makeCardStack = (cards, update) => {
   const allCards = cards.map(card => {
     switch (card["board_type"]) {
       case "expense":
         return (
-          <NavLink to={`/expense/${card["id"]}`}>
+         
             <div className="card my-2 mx-1">
               <div className="card-body">
+              <NavLink to={`/expense/${card["id"]}`}>
                 <FontAwesomeIcon icon="home" className="icons pb-2" />
                 <h5 className="card-title">{card["name"]}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">
                   budgeted: ${card["goal"]}
                 </h6>
+                </NavLink>
                 <BarGraphs card={card} />
+
+                <Popup
+                  trigger={
+                    <button type="button" className="btn btn-lg category-btn">
+                      + Entry
+                    </button>
+                  }
+                  modal
+                  closeOnDocumentClick>
+                  <NewExpenseEntry category={card} update={update} />
+                </Popup>
+
               </div>
             </div>
-          </NavLink>
+       
         );
     }
   });
@@ -35,7 +50,7 @@ class ExpenseBoard extends Component {
     return (
       <div className="expense-board py-4 text-center">
         <h4 className="py-3">Expense Boards</h4>
-        <div>{makeCardStack(props.categories)}</div>
+        <div>{makeCardStack(props.categories, props.update)}</div>
         <Popup
           trigger={
             <button type="button" className="btn btn-lg category-btn">
