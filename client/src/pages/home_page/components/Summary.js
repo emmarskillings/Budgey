8f2f4a3fbf2be25fd2f983af508b11bc8323ff61
Eric Joo <ericjaeminjoo@gmail.com>
2018-08-02 19:18:57 +0000

    Modify chartjs and fix layout

--- client/src/pages/home_page/components/Summary.js
+++ client/src/pages/home_page/components/Summary.js
@@ -75,8 +75,16 @@ class CategoryContainer extends Component {
       responsive: true,
   	  legend: {
   	  	"display": true,
-  	  	"position":"right",
-  	  },
+        "position": "right"
+      },
+      layout: {
+        padding: {
+          left: 0,
+          right: 300,
+          top: 0,
+          bottom: 0
+        }
+      }
   	}
 
     const data = {
@@ -88,10 +96,11 @@ class CategoryContainer extends Component {
         }
 
 	  return(
-	  	<div style={{'padding-right':200}}>
-		    <h4 align='left'>Spending Summary</h4>
-		  	<Doughnut data={data} options={options} height={70}/>
-				<br/>
+	  	<div className="summary text-center">
+        <h4 className="py-3">Expense</h4>
+        <div id="pie-chart">
+		  	  <Doughnut data={data} options={options} height={85}/>
+        </div>
 	  	</div>
 	  	)
 
