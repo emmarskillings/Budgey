dad01cd84d91247c3389cfd62fc3858dcee66d12
Eric Joo <ericjaeminjoo@gmail.com>
2018-08-03 15:01:32 +0000

    UAdd flexbox for layout and alignment

--- client/src/pages/home_page/components/Rundown.js
+++ client/src/pages/home_page/components/Rundown.js
@@ -16,37 +16,39 @@ class Rundown extends Component {
     });
 
     const amount_left = total_income - total_expenses;
-    let color = "green";
+    let color = "rgb(139, 179, 88)";
     if (amount_left < 0) {
-      color = "red";
+      color = "rgb(250, 77, 65)";
     }
 
     return (
-      <div className="rundown text-center mt-3">
-        <h4>You've budgeted...</h4>
-        <div className="rundown-income row justify-content-between">
-          <div className="col-5">
+      <div className="rundown mt-3">
+        <h4 className="text-center">You've budgeted...</h4>
+        <div className="rundown-income d-flex bd-highlight mx-5">
+          <div className="mr-auto bd-highlight">
             <h6>Income</h6>
           </div>
-          <div className="col-5" id="total-income">
-            <NumberFormat value=${total_income} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+          <div className="ml-auto bd-highlight" id="total-income">
+            <NumberFormat value={`${total_income}`} displayType={'text'} thousandSeparator={true} prefix={'$'} />
           </div>
         </div>
-        <div className="rundown-spending row justify-content-between">
-          <div className="col-5">
+        <div className="rundown-spending d-flex bd-highlight mx-5">
+          <div className="mr-auto bd-highlight">
             <h6>Spending</h6>
           </div>
-          <div className="col-5" id="total-expenses">
-            <NumberFormat value=${total_expenses} displayText={'text'} thousandSeparator={true} prefix={'-$'} />
+          <div className="ml-auto bd-highlight" id="total-expenses">
+            <NumberFormat value={`${total_expenses}`} displayType={'text'} thousandSeparator={true} prefix={'-$'} />            
           </div>
         </div>
-        <hr />
-        <div className="rundown-leftover row justify-content-between">
-          <div className="col-5">
+        <hr width="80%" />
+        <div className="rundown-leftover d-flex bd-highlight mx-5">
+          <div className="mr-auto bd-highlight">
             <h6>Leftover</h6>
           </div>
-          <div className="col-5">
-            <p id="amount-left" style={{ color: color }}>${amount_left}</p>
+          <div className="ml-auto bd-highlight">
+            <p id="amount-left" style={{ color: color }}>
+              <NumberFormat value={`${amount_left}`} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+            </p>
           </div>
         </div>
       </div>
