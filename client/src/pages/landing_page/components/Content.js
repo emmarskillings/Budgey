150e6d57ed63c8149b26d0a2344a8d6e3d882398
Eric Joo <ericjaeminjoo@gmail.com>
2018-08-07 22:05:10 +0000

    Modify landing page component

--- client/src/pages/landing_page/components/Content.js
+++ client/src/pages/landing_page/components/Content.js
@@ -6,15 +6,17 @@ class Content extends Component {
   render() {
     return (
       <div className="landing-content">
-        <h1>Welcome to Budgey!</h1>
-        <h5 className="tagline">Your friendly budgeting buddy</h5>
-        <p className="content-message">
-          Are you a first time budgeter? <a href="/signup">Register now.</a>
-        </p>
-        <p className="content-message">
-          Been here before? <a href="/login">Log in</a> to keep budgeting!
-        </p>
-        <LoginWithGoogle />
+        <div className="description">
+          <h1>Welcome to Budgey!</h1>
+          <h5 className="tagline">Your friendly budgeting buddy</h5>
+          <p className="content-message">
+            Are you a first time budgeter? <a href="/signup">Register now.</a>
+          </p>
+          <p className="content-message">
+            Been here before? <a href="/login">Log in</a> to keep budgeting!
+          </p>
+          <LoginWithGoogle />
+        </div>
       </div>
     );
   }
